bfdb28f87dd9caa55636f9a65da923e6b648b5ee
Stella Diaz <electi@yzu.moe>
2026-02-21 19:57:51 -0500

    Q25: Initial extraction from  14/20240427/UP1v
    Signed-off-by: Stella Diaz <electi@yzu.moe>

---
+++ Android.mk
@@ -0,0 +1,20 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),Q25)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,e9a5cb8eadd7b33c0d9493c7ce064571b5d6bf97)
+$(call add-radio-file-sha1-checked,radio/gz.img,cbe7af74de2453cbf707471603a2dc80303dc9c0)
+$(call add-radio-file-sha1-checked,radio/lk.img,e433014438900a7e668ce2ac9e2650e6984e362f)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,e7d9bf70c657e2e30af15e06391fae2eb7b9925c)
+$(call add-radio-file-sha1-checked,radio/md1img.img,af9d672bac61909a3959c1c0f95372666337372d)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,723128cde784262583051e3993a30c6c491a9d44)
+$(call add-radio-file-sha1-checked,radio/scp.img,ef6de8fcd01a12c0dff2242fe59653083d50a9c0)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,13a50887bcb5f5ff4d3c5563cf00593f159aa891)
+$(call add-radio-file-sha1-checked,radio/sspm.img,18c271af4c5b4fc70a0fef0e2a0b68e6120edf63)
+$(call add-radio-file-sha1-checked,radio/tee.img,ff63583ae832ceffd96568407fd29c6794db10ff)
+
+endif
